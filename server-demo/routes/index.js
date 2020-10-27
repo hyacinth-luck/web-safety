23134c50f03a62b0c26cfcf5ebf61bd8a77d2d88
dusiyong <dusiyong@yy.com>
2020-10-27 23:19:19 +0800

    edit

--- server-demo/routes/index.js
+++ server-demo/routes/index.js
@@ -8,16 +8,23 @@ router.get('', function(req, res, next) {
 });
 router.get('/badUrl', function(req, res, next) {
   console.log('req:',req)
-  res.render('data', { title: 'xss-badUrl' });
+  // res.render('data', { title: 'xss-badUrl' });
   res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
   res.write('<script>alert("反射型 XSS 攻击---恶意链接" )</script>');
   res.end();
 });
 
-// router.post('/save', function(req, res, next) {
-//   console.log('req:',req)
-//   res.render('data', { title: 'xss攻击' });
-//   res.end({data:'success'});
-// });
+router.post('/save', function(req, res, next) {
+  console.log('req:',req)
+  userInput = req.body
+  // res.render('data', { title: 'xss攻击' });
+  res.send({data:'success'});
+});
+router.get('/getCommit', function(req, res, next) {
+  // console.log('req:',req)
+  // userInput = req.body
+  // res.render('data', { title: 'xss攻击' });
+  res.send({data:userInput});
+});
 
 module.exports = router;
