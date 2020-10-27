6f5f704d762f1a724e6b9322b7249469a202c740
dusiyong <dusiyong@yy.com>
2020-10-27 20:52:04 +0800

    edit

--- server-demo/routes/index.js
+++ server-demo/routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
+let userInput = '';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('', function(req, res, next) {
+  res.render('xss', { title: 'xss' });
+});
+router.get('/badUrl', function(req, res, next) {
+  console.log('req:',req)
+  res.render('data', { title: 'xss-badUrl' });
+  res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
+  res.write('<script>alert("反射型 XSS 攻击---恶意链接" )</script>');
+  res.end();
 });
 
-router.get('/dsy', function(req, res, next) {
-  res.render('dsy', { title: 'dsy' });
-});
+// router.post('/save', function(req, res, next) {
+//   console.log('req:',req)
+//   res.render('data', { title: 'xss攻击' });
+//   res.end({data:'success'});
+// });
 
 module.exports = router;
